d0a70d1e7e1da1fd624de25c5f2be7344281f9c6
ibnuruslan368-boop <ibnuruslan368@gmail.com>
2026-04-20 13:29:39 +0800

    Pertemuan1. Py

---
+++ pertemuan1.py (genap)-1.py
@@ -0,0 +1,13 @@
+#("NAMA :Rino ibnu ruslan madar ladoangin")
+#("NIM  :25241081")
+#("KELAS:PTI C")
+
+nama = input("masukan nama anda :")
+if nama=="Rino ibnu ruslan madar ladoangin" :
+    print("selamat datang Rino NIM anda adalah")
+    print("25241081")
+
+else :
+    print("nama salah")
+    print("anda tidak terdaftar")
+print("program selesai")
