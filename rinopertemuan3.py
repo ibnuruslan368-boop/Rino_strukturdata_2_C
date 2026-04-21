fb923e698c8de8efbf1d29161b5c82c1db2ea746
ibnuruslan368-boop <ibnuruslan368@gmail.com>
2026-04-21 10:16:32 +0800

    Add files via upload

---
+++ rinopertemuan3.py
@@ -0,0 +1,13 @@
+#("NAMA :Rino I.R.M Ladoangin")
+#("NIM  :25241081")
+#("KELAS:PTI C")
+
+while True:
+    angka = int(input("Masukkan angka: "))
+    if angka < 0:
+        print("\033[91mHarus positif!\033[0m")
+
+    
+    else:
+        print("Angka yang dimasukkan:", angka)
+        break
