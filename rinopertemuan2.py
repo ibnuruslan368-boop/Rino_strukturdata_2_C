849860d067a9cb2a22a6715818e2437535d4f3dc
ibnuruslan368-boop <ibnuruslan368@gmail.com>
2026-04-21 10:16:03 +0800

    Add files via upload

---
+++ rinopertemuan2.py
@@ -0,0 +1,15 @@
+#("NAMA :Rino I.R.M Ladoangin")
+#("NIM  :25241081")
+#("KELAS:PTI C")
+
+angka = [0,1,2,3,4,5] 
+for i in angka : 
+    print(f"nilai i = {i}")
+    print("rino")
+print("program selesai")
+
+angka = range (1,50) 
+for i in angka :
+    print (f"nilaii i = {i}")
+    print("rino")
+print("program selesai")
